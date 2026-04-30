1663e030887c6240fe39e4b2b27b93035a9c87a4
Mohab Ahmed <M.Ahmed-22@student.tudelft.nl>
2026-04-30 18:16:30 +0200

    removed skills, shared & integration folders

--- integration/sidecar/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Sidecar daemon that exposes AgentChannel over a Unix-domain JSON-RPC socket."""
-
-from integration.sidecar.server import Sidecar
-from integration.sidecar.socket import UnixSocketServer
-from integration.sidecar.lifecycle import run
-
-__all__ = ["Sidecar", "UnixSocketServer", "run"]
